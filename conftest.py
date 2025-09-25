3085ecddd0dcca4659c72e5b3d0109e4bbb4c26e
Victor Dias <victorrebelodias@gmail.com>
2025-09-25 17:40:58 -0300

    alterações para tests

--- conftest.py
+++ conftest.py
@@ -4,9 +4,10 @@ from selenium import webdriver
 
 @pytest.fixture(scope="module")
 def browser():
-	chrome_path = "/home/victor/Scripting/pythonjenkins/dash/chrome-linux64/chrome"
 	chrome_options = webdriver.ChromeOptions()
-	chrome_options.binary_location = chrome_path
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--disable-gpu")
+	
 	driver = webdriver.Chrome(options=chrome_options)
 
 	yield driver
--- tests/test_home.py
+++ tests/test_home.py
@@ -1,7 +1,7 @@
 import time
 
 def test_home(browser):
-	url = "http://127.0.0.1:8080"
+	url = "http://127.0.0.1:8081"
 	browser.get(url)
 	time.sleep(10)
 	assert browser.title == "Dash"
